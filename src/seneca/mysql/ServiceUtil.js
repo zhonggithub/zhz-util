cfc1582e77cc2b31ac543ec9e7acafbd4db98a7a
Zz <quitjie@gmail.com>
2020-06-20 19:08:37 +0800

    fix ServiceUtil.js

--- src/seneca/mysql/ServiceUtil.js
+++ src/seneca/mysql/ServiceUtil.js
@@ -5,7 +5,7 @@
  * Created Date: 2020-06-13 19:55:48
  * Author: Zz
  * -----
- * Last Modified: 2020-06-17 22:17:05
+ * Last Modified: 2020-06-20 17:43:57
  * Modified By: Zz
  * -----
  * Description:
@@ -15,19 +15,20 @@ const ServiceUtilBase = require('../ServiceUtilBase');
 
 class ServiceUtil extends ServiceUtilBase {
   convertQueryCriteria(criteria) {
-    let tmpCriteria = util.convertQueryCriteria(criteria, 'mysql');
-    const dbCriteria = tmpCriteria.dstCriteria;
-    tmpCriteria = tmpCriteria.sourceCriteria;
-    for (const condition in tmpCriteria) {
-      if (Object.prototype.hasOwnProperty.call(criteria, condition)) {
-        switch (condition) {
-          default:
-            dbCriteria[condition] = tmpCriteria[condition];
-            break;
-        }
+    if (criteria && criteria.where) {
+      const tmpCriteria = util.convertQueryCriteria(criteria.where, 'mysql');
+      criteria.where = {
+        ...tmpCriteria.dstCriteria,
+        ...tmpCriteria.sourceCriteria,
       }
+      return criteria;
+    } else {
+      const tmpCriteria = util.convertQueryCriteria(criteria, 'mysql');
+      return {
+        ...tmpCriteria.dstCriteria,
+        ...tmpCriteria.sourceCriteria,
+      };
     }
-    return dbCriteria;
   }
 }
 
