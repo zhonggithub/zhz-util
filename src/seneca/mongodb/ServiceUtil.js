f78ab3392d67649edc9af1669693a4a24c7f1278
Zz <quitjie@gmail.com>
2020-06-26 14:24:19 +0800

    fix

--- src/seneca/mongodb/ServiceUtil.js
+++ src/seneca/mongodb/ServiceUtil.js
@@ -5,7 +5,7 @@
  * Created Date: 2020-06-14 09:08:48
  * Author: Zz
  * -----
- * Last Modified: 2020-06-21 17:20:14
+ * Last Modified: 2020-06-26 14:21:44
  * Modified By: Zz
  * -----
  * Description:
@@ -14,8 +14,8 @@ const util = require('../../util');
 const ServiceUtilBase = require('../ServiceUtilBase');
 
 class ServiceUtil extends ServiceUtilBase {
-  convertQueryCriteria(criteria) {
-    const tmpCriteria = util.convertQueryCriteria(criteria, 'mongodb');
+  convertQueryCriteria(criteria, handle) {
+    const tmpCriteria = util.convertQueryCriteria(criteria, 'mongodb', handle);
     return {
       ...tmpCriteria.dstCriteria,
       ...tmpCriteria.sourceCriteria,
--- src/seneca/mysql/ServiceUtil.js
+++ src/seneca/mysql/ServiceUtil.js
@@ -5,7 +5,7 @@
  * Created Date: 2020-06-13 19:55:48
  * Author: Zz
  * -----
- * Last Modified: 2020-06-20 17:43:57
+ * Last Modified: 2020-06-26 14:20:44
  * Modified By: Zz
  * -----
  * Description:
@@ -14,16 +14,16 @@ const util = require('../../util');
 const ServiceUtilBase = require('../ServiceUtilBase');
 
 class ServiceUtil extends ServiceUtilBase {
-  convertQueryCriteria(criteria) {
+  convertQueryCriteria(criteria, handle) {
     if (criteria && criteria.where) {
-      const tmpCriteria = util.convertQueryCriteria(criteria.where, 'mysql');
+      const tmpCriteria = util.convertQueryCriteria(criteria.where, 'mysql', handle);
       criteria.where = {
         ...tmpCriteria.dstCriteria,
         ...tmpCriteria.sourceCriteria,
       }
       return criteria;
     } else {
-      const tmpCriteria = util.convertQueryCriteria(criteria, 'mysql');
+      const tmpCriteria = util.convertQueryCriteria(criteria, 'mysql', handle);
       return {
         ...tmpCriteria.dstCriteria,
         ...tmpCriteria.sourceCriteria,
