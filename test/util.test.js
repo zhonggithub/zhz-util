1bc7ac34acbe2f2b8ff5fb25f0cad4b73aca8521
Zz <quitjie@gmail.com>
2020-07-22 16:14:00 +0800

    fix

--- test/util.test.js
+++ test/util.test.js
@@ -5,19 +5,20 @@
  * Created Date: 2020-06-27 00:57:14
  * Author: Zz
  * -----
- * Last Modified: 2020-06-27 00:59:49
+ * Last Modified: 2020-07-22 16:13:49
  * Modified By: Zz
  * -----
  * Description:
  */
 const util = require('../src/util')
 
+// true
 console.log(util.isValueOf('a', 'a'))
 console.log(util.isValueOf(1, 1))
 console.log(util.isValueOf('a', ['a']))
 console.log(util.isValueOf('a', { b: 'a' }))
 
-
+// false
 console.log(util.isValueOf('a', 'b'))
 console.log(util.isValueOf(1, 2))
 console.log(util.isValueOf('a', ['bb']))
