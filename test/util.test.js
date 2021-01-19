2b3d2c3e214afdb420d3afec6163e0041a9f60b5
Zz <quitjie@gmail.com>
2021-01-19 21:32:23 +0800

    fix:增加测试用例

--- test/util.test.js
+++ test/util.test.js
@@ -5,7 +5,7 @@
  * Created Date: 2020-06-27 00:57:14
  * Author: Zz
  * -----
- * Last Modified: Mon Jan 18 2021
+ * Last Modified: Tue Jan 19 2021
  * Modified By: Zz
  * -----
  * Description:
@@ -77,3 +77,29 @@ console.log(util.convertObjKey({
   }
   return false
 }))
+
+const tmp1 = {
+  "object": "queryEntryUser",
+  "status": "succeeded",
+  "prod_mode": "true",
+  "request_id": "",
+  "test_api_key": "api_test_34",
+  "live_api_key": "api_live_7b",
+  "app_id_list": [
+    {
+      "app_id": "app_0b05",
+      "app_name": "test_1",
+      "cre_ts": 1611044354000,
+      "cre_user": "0804",
+      "id": 74304,
+      "mer_cust_id": "0864",
+      "remark": "",
+      "stat": "N",
+      "upd_ts": 1611044354000,
+      "upd_user": "0864"
+    }
+  ],
+  "sign_view_url": "https://www.baidu.com",
+  "login_pwd": ""
+}
+console.log(util.convertObjKey(tmp1, 'hump'))
